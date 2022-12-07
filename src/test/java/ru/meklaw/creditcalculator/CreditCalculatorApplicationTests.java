31a35177be267f256d3e7cd74f9efadf8213aa77
Никита Ульянов <nik.ule@yandex.ru>
2022-12-08 00:22:24 +0300

    Initial Commit

---
+++ src/test/java/ru/meklaw/creditcalculator/CreditCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.meklaw.creditcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
